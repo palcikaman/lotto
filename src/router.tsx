2ec8ed32650a8067a96618995bf1fe0d4dc17b0f
palcikaman <palcikaman@gmail.com>
2023-12-20 15:46:51 +0100

    route fix

--- src/router.tsx
+++ src/router.tsx
@@ -5,31 +5,35 @@ import { Player } from './pages/player/Player.tsx';
 import { House } from './pages/house/House.tsx';
 import { Info } from './pages/Info.tsx';
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: (
-      <Layout>
-        <Outlet />
-      </Layout>
-    ),
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: '/player',
-        element: <Player />,
-      },
-      {
-        path: '/house',
-        element: <House />,
-      },
-      {
-        path: '/info',
-        element: <Info />,
-      },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+
+      element: (
+        <Layout>
+          <Outlet />
+        </Layout>
+      ),
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: '/player',
+          element: <Player />,
+        },
+        {
+          path: '/house',
+          element: <House />,
+        },
+        {
+          path: '/info',
+          element: <Info />,
+        },
+      ],
+    },
+  ],
+  { basename: '/lotto' }
+);
